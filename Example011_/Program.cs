8ac462092a34d0bf5c5ad84fc9ca4fa8a187b8bc
Anna <azavas@yandex.ru>
2022-10-11 09:24:16 +0300

    2 семинар

---
+++ Example011_/Program.cs
@@ -0,0 +1,52 @@
+﻿void FillArray(int[] collecnion)
+{
+    int length = collecnion.Length;
+    int index = 0;
+    while (index<length)
+    {
+        collecnion[index] = new Random().Next (1,10);
+        
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position< count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+
+}
+
+
+
+int IndexOf(int[] collecnion, int find)
+{
+    int count = collecnion.Length;
+    int index=0;
+    int position = 0;
+
+    while (index < count)
+    {
+        if(collecnion[index] == find)
+        {
+            position = index;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int [10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
+
